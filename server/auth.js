df1843916fd59655b8e5a6039e8f2abadaa8616b
sgtpeter30 <68299466+sgtpeter30@users.noreply.github.com>
2024-02-06 20:32:35 +0100

    Delete server/auth.js

--- server/auth.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-authData = {
-  mongooseAdress: 'mongodb+srv://blogAdmin:zsUc2EMu4oAw7TOH@blog-app.cxcqp.mongodb.net/LibraryApp?retryWrites=true&w=majority',
-  secString: 'Mistrzowie-Polskiej-Fantastyki-kolekcja-60-tomów',
-}
-module.exports = authData;
