21cb750d86fc890bd93e94de5aecf72b3a635c91
sgtpeter30 <sgtpeter30@gmail.com>
2024-11-01 21:32:23 +0100

    Update Angular CLI

--- src/athenaeum/app.module.ts
+++ src/athenaeum/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { AuthInterceptorService } from 'src/shared/services/user-based/auth-interceptor.service';
 import { MenuComponent } from "./menu/menu.component";
 
@@ -12,21 +12,15 @@ export const environment = {
   apiUrl: "http://localhost:3200"
 };
 
-@NgModule({
-    declarations: [
+@NgModule({ declarations: [
         AppComponent
     ],
-    providers: [
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        AppRoutingModule,
+        MenuComponent], providers: [
         { provide: "BASE_API_URL", useValue: environment.apiUrl },
         { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptorService, multi: true },
-        provideAnimationsAsync()
-    ],
-    bootstrap: [AppComponent],
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        HttpClientModule,
-        MenuComponent
-    ]
-})
+        provideAnimationsAsync(),
+        provideHttpClient(withInterceptorsFromDi())
+    ] })
 export class AppModule { }
